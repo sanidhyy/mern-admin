beeb80248e683297b43331f1cc7c4284a0b838c7
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-17 17:38:36 +0530

    navbar created

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
+import {
+  AppBar,
+  useTheme,
+  Toolbar,
+  IconButton,
+  InputBase,
+} from "@mui/material";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrowDropDownOutlined,
+} from "@mui/icons-material";
+
+import FlexBetween from "components/FlexBetween";
+import profileImage from "assets/profile.jpeg";
+
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+
+  return (
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+      }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        {/* Left Side */}
+        <FlexBetween>
+          {/* Sidebar Menu */}
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+            <MenuIcon />
+          </IconButton>
+
+          {/* Search */}
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+
+        {/* Right Side */}
+        <FlexBetween gap="1.5rem">
+          {/* Dark/Light Mode */}
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
+
+          {/* Settings */}
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
+          </IconButton>
+        </FlexBetween>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
