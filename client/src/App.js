291548c1f2e5c9ec7de37f55399c461f70da0a78
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-17 14:00:51 +0530

    material ui setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
 
 // App
 const App = () => {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-      <h1>Mern Dashboard</h1>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 };
