0fedb86a8999e9965ec7392f4207b7e5b22666f2
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-17 12:53:12 +0530

    configuration created

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import clientRoutes from "./routes/client.js"
+import generalRoutes from "./routes/general.js"
+import managementRoutes from "./routes/management.js"
+import salesRoutes from "./routes/sales.js"
+
+// Configuration
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// Routes
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes)
+app.use("/management", managementRoutes)
+app.use("/sales", salesRoutes)
