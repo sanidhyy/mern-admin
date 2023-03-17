1a52d08062006ae6ba48a2bf3720e8a6f975def7
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-17 17:38:16 +0530

    flex between style added

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+const { styled } = require("@mui/material");
+const { Box } = require("@mui/system");
+
+const FlexBetween = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+export default FlexBetween;
