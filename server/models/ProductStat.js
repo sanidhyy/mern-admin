144e9824bd39fc48fec2243b82f8d72d4dccdd52
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-18 14:35:49 +0530

    product schemas created

---
+++ server/models/ProductStat.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const ProductStatSchema = new mongoose.Schema(
+  {
+    productId: String,
+    yearlySalesTotal: Number,
+    yearlyTotalSoldUnits: Number,
+    year: Number,
+    monthlyData: [
+      {
+        month: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+    dailyData: {
+      data: String,
+      totalSales: Number,
+      totalUnits: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const ProductStat = mongoose.model("ProductStat", ProductStatSchema);
+
+export default ProductStat;
