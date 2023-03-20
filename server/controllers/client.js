90c3a4bc4c0f708e8ca46955d93e7f52c45805c3
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-20 13:56:17 +0530

    customers server side api done

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,6 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
 
 export const getProducts = async (_, res) => {
   try {
@@ -18,6 +19,15 @@ export const getProducts = async (_, res) => {
     );
 
     res.status(200).json(productsWithStats);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
--- server/routes/client.js
+++ server/routes/client.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { getProducts } from "../controllers/client.js";
+import { getProducts, getCustomers } from "../controllers/client.js";
 
 const router = express.Router();
 
 router.get("/products", getProducts);
+router.get("/customers", getCustomers);
 
 export default router;
