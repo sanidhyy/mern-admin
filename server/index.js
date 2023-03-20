af1aee30d2946eaa43070a723958b4c2c732766a
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-20 17:42:33 +0530

    overall stat api setup

--- server/index.js
+++ server/index.js
@@ -16,11 +16,13 @@ import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
+  dataOverallStat,
 } from "./data/index.js";
 
 // Configuration
@@ -56,5 +58,6 @@ mongoose
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
+    // OverallStat.insertMany(dataOverallStat);
   })
   .catch((error) => console.log(`${error} did not connect.`));
---
+++ server/models/OverallStat.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const OverallStatSchema = new mongoose.Schema(
+  {
+    totalCustomers: Number,
+    yearlySalesTotal: Number,
+    yearlyTotalSoldUnits: Number,
+    monthlyData: [
+      {
+        month: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+    dailyData: [
+      {
+        data: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+    salesByCategory: {
+      type: Map,
+      of: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const OverallStat = mongoose.model("OverallStat", OverallStatSchema);
+
+export default OverallStat;
