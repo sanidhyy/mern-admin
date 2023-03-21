e2f06967fb401f13ed5ee713c6c99086afcb2816
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-21 17:39:41 +0530

    dashboard api endpoint setup

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,10 +1,60 @@
 import User from "../models/User.js";
+import OverallStat from "../models/OverallStat.js";
+import Transaction from "../models/Transaction.js";
 
 export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
     res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getDashboardStats = async (_, res) => {
+  try {
+    // Hardcoded Values
+    const currentMonth = "November";
+    const currentYear = 2021;
+    const currentDate = "2021-11-15";
+
+    // Recent Transactions
+    const transactions = await Transaction.find()
+      .limit(50)
+      .sort({ createdAt: -1 });
+
+    // Overall Stats
+    const overallStat = await OverallStat.find({ year: currentYear });
+
+    const {
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+    } = overallStat[0];
+
+    // This month stats
+    const thisMonthStats = overallStat[0].monthlyData.find(({ month }) => {
+      return month === currentMonth;
+    });
+
+    // today stats
+    const todayStats = overallStat[0].dailyData.find(({ date }) => {
+      return date === currentDate;
+    });
+
+    res.status(200).json({
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+      thisMonthStats,
+      todayStats,
+      transactions,
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
