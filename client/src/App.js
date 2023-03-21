13dd9e76f77e85e83ded73468c92389e4dc65b73
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-21 14:39:49 +0530

    breakdown page done

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Geography from "scenes/geography";
 import Overview from "scenes/overview";
 import Daily from "scenes/daily";
 import Monthly from "scenes/monthly";
+import Breakdown from "scenes/breakdown";
 
 // App
 const App = () => {
@@ -36,6 +37,7 @@ const App = () => {
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
               <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
             </Route>
           </Routes>
         </ThemeProvider>
