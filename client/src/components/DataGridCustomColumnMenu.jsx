0b12ff24d43524fe69f25949736b1b165a735177
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-21 15:32:04 +0530

    mui data grid column created

---
+++ client/src/components/DataGridCustomColumnMenu.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+  GridColumnMenuContainer,
+  GridFilterMenuItem,
+  HideGridColMenuItem,
+} from "@mui/x-data-grid";
+
+const CustomColumnMenu = (props) => {
+  const { hideMenu, currentColumn, open } = props;
+  return (
+    <GridColumnMenuContainer
+      hideMenu={hideMenu}
+      currentColumn={currentColumn}
+      open={open}
+    >
+      <GridFilterMenuItem onClick={hideMenu} column={currentColumn} />
+      <HideGridColMenuItem onClick={hideMenu} column={currentColumn} />
+    </GridColumnMenuContainer>
+  );
+};
+
+export default CustomColumnMenu;
