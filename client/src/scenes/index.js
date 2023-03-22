69380973b51b700c0e54f1df7cf5001d5707d614
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-22 22:32:28 +0530

    added scenes index file

---
+++ client/src/scenes/index.js
@@ -0,0 +1,31 @@
+// Contains scenes to be exported in other files
+// ! Don't remove anything from here
+
+import Layout from "./layout";
+import Dashboard from "./dashboard";
+import Products from "./products";
+import Customers from "./customers";
+import Transactions from "./transactions";
+import Geography from "./geography";
+import Overview from "./overview";
+import Daily from "./daily";
+import Monthly from "./monthly";
+import Breakdown from "./breakdown";
+import Admin from "./admin";
+import Performance from "./performance";
+
+// export scenes
+export {
+  Layout,
+  Dashboard,
+  Products,
+  Customers,
+  Transactions,
+  Geography,
+  Overview,
+  Daily,
+  Monthly,
+  Breakdown,
+  Admin,
+  Performance,
+};
