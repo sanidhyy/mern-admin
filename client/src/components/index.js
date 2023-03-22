ada60a7ef5132485881fd1791645b3b984913e5e
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-22 22:32:04 +0530

    components index file added

---
+++ client/src/components/index.js
@@ -0,0 +1,25 @@
+// Contains components to be exported in other files
+// ! Don't remove anything from here
+
+import BreakdownChart from "./BreakdownChart";
+import CustomColumnMenu from "./DataGridCustomColumnMenu";
+import DataGridCustomToolbar from "./DataGridCustomToolbar";
+import FlexBetween from "./FlexBetween";
+import Header from "./Header";
+import Navbar from "./Navbar";
+import OverviewChart from "./OverviewChart";
+import Sidebar from "./Sidebar";
+import StatBox from "./StatBox";
+
+// export components
+export {
+  BreakdownChart,
+  CustomColumnMenu,
+  DataGridCustomToolbar,
+  FlexBetween,
+  Header,
+  Navbar,
+  OverviewChart,
+  Sidebar,
+  StatBox,
+};
