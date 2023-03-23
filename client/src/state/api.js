7cc504c8a7dde5988e59f725ee2555126daaa0a1
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-23 14:52:21 +0530

    api fallback added

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -2,7 +2,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Backend Api
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }), // base url
+  baseQuery: fetchBaseQuery({
+    baseUrl:
+      process.env.REACT_APP_BASE_URL ||
+      "http://localhost:5001",
+  }), // base url
   reducerPath: "adminApi",
   // tags
   tagTypes: [
